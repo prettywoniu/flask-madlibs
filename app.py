f66c2914e225786183dfe66fcecca64ce22fc9f5
prettywoniu <prettywoiu@hotmail.com>
2023-12-20 23:13:44 -0800

    first version

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from random import randint
+#from stories import story
+from stories_further import stories
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "xxm123"
+
+debug = DebugToolbarExtension(app)
+
+
+# @app.route('/')
+# def ask_questions():
+#     """Generate and show form to ask words"""
+
+#     prompts = story.prompts
+#     return render_template("form.html", prompts=prompts)
+
+
+# @app.route('/story')
+# def show_story():
+#     """Show story result"""
+    
+#     text = story.generate(request.args)
+#     return render_template("story.html", text=text)
+
+@app.route("/")
+def ask_story():
+    """Show list-of-stories form."""
+
+    return render_template("select-story.html",
+                           stories=stories.values())
+
+
+@app.route("/questions")
+def ask_questions():
+    """Generate and show form to ask words."""
+
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+
+    prompts = story.prompts
+
+    return render_template("questions.html",
+                           story_id=story_id,
+                           title=story.title,
+                           prompts=prompts)
+
+
+@app.route("/story")
+def show_story():
+    """Show story result."""
+
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+
+    text = story.generate(request.args)
+
+    return render_template("story.html",
+                           title=story.title,
+                           text=text)
